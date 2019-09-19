20cab3f60d9ac2d7b5b834b3f1c3f5469acca2c5
TiMaTiFeY <timatifey.001@yandex.ru>
2019-09-20 02:20:42 +0300

    Исправлен canBuildFrom (lesson 5)

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -218,7 +218,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean =
-    word.toLowerCase().toSet().intersect(chars.map { it.toLowerCase() }.toSet() ) == word.toLowerCase().toSet()
+    word.toLowerCase().toSet().intersect(chars.map { it.toLowerCase() }.toSet()) == word.toLowerCase().toSet()
 
 /**
  * Средняя
