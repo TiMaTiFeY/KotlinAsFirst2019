dfb6990ca13eee52e37c503afffb2a77e4a2a0c5
TiMaTiFeY <timatifey.001@yandex.ru>
2019-10-29 23:53:17 +0300

    7 lesson fix

--- test/lesson7/task1/Tests.kt
+++ test/lesson7/task1/Tests.kt
@@ -192,12 +192,19 @@ Basic, Ruby, Swift.
     fun transliterate() {
         transliterate(
             "input/trans_in1.txt",
-            mapOf('L' to ""),
+            mapOf('з' to "zz", 'р' to "r", 'д' to "d", 'й' to "y", 'М' to "m", 'и' to "yy", '!' to "!!!"),
             "temp.txt"
         )
-        assertFileContent("temp.txt", "")
+        assertFileContent("temp.txt", "Zzdrавствуy,\nmyyr!!!")
         File("temp.txt").delete()
 
+        transliterate(
+            "input/trans_in1.txt",
+            mapOf('з' to "zZ", 'р' to "r", 'д' to "d", 'й' to "y", 'М' to "m", 'и' to "YY", '!' to "!!!"),
+            "temp.txt"
+        )
+        assertFileContent("temp.txt", "Zzdrавствуy,\nmyyr!!!")
+        File("temp.txt").delete()
     }
 
     @Test
